94474e5ccc32eee784079bb1d43fb7eaa141468e
Chen Zhe <chenzhesg@gmail.com>
2022-05-05 22:14:09 -0400

    save plt to file

--- py_data/data_interface.py
+++ py_data/data_interface.py
@@ -8,9 +8,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def pack_bin_file(data, prefix):
-  with open(f"{prefix}_{data.shape[0]}x{data.shape[1]}.bin", "wb") as file:
+  filename = f"{prefix}_{data.shape[0]}x{data.shape[1]}.bin"
+  with open(filename, "wb") as file:
     file.write(struct.pack('2i', *data.shape))
     file.write(data.astype(np.float32).tobytes())
+  return filename
 
 def load_bin_file(path):
   with open(path, "rb") as file:
@@ -18,10 +20,15 @@ def load_bin_file(path):
   data_shape = struct.unpack('2i', data[:8])
   return np.frombuffer(data[8:], dtype=np.float32).reshape(data_shape)
 
-def visualize_tsne_result(tsne_embedded, labels=None, dot_size=8):
-  fig = plt.figure(1, (12., 10.))
+def visualize_tsne_result(tsne_embedded, labels=None, dot_size=8, filename=None, title=None):
+  fig = plt.figure(1, (12., 10.), dpi=150)
   sc = plt.scatter(tsne_embedded[:, 0], tsne_embedded[:, 1], alpha=0.8, s=dot_size,
               c=labels, cmap="plasma")
   legend1 = plt.legend(*sc.legend_elements(),
                       bbox_to_anchor=(1.04,1), loc="upper left", title="Classes")
   fig.add_artist(legend1)
+  if title is not None:
+    plt.title(title)
+  if filename is not None:
+    plt.savefig(filename, bbox_inches="tight")
+    plt.close()
